2c45b2f80681a6b533b76e66bcd832baa799e75f
daveS <davesimpcontrol@gmail.com>
2023-05-14 09:58:48 -0600

    added all required section files (empty)

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          thiswill <code>src/App.js</code> portfolio.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  </div>
   );
 }
 
