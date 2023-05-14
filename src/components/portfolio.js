5a40f395953bfca94c3f1d22a6e1a4f493ada884
daveS <davesimpcontrol@gmail.com>
2023-05-14 13:14:32 -0600

    added a place to put a title, image, and link for each project as boilerplate

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,3 +1,17 @@
 import React from 'react';
 
-// export default Portfolio;
+function Portfolio() {
+    return (
+      <section id="portfolio">
+        <h2>Portfolio</h2>
+        <div className="project-container">
+          <Project title="Project 1" image="path/to/image1.jpg" deployed="https://deployed-app-url.com"/>
+          <Project title="Project 2" image="path/to/image2.jpg" deployed="https://deployed-app-url.com"/>
+          <Project title="Project 3" image="path/to/image3.jpg" deployed="https://deployed-app-url.com"/>
+          <Project title="Project 4" image="path/to/image4.jpg" deployed="https://deployed-app-url.com"/>
+        </div>
+      </section>
+    );
+}
+
+export default Portfolio;
