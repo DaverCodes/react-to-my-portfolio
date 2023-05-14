803541692bde084426b53c8f2e9c21aafae442eb
daveS <davesimpcontrol@gmail.com>
2023-05-14 10:30:08 -0600

    all new section.js files put into components folder to provide easier navigation. all import and exports added to new files and APP.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
