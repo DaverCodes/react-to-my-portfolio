b9a955d0ef874e01f0599542ef49085dcb07177b
daveS <davesimpcontrol@gmail.com>
2023-05-14 13:08:29 -0600

    added an image and paragraph to the about me section

--- src/components/about.js
+++ src/components/about.js
@@ -1,3 +1,14 @@
 import React from "react";
 
-// export default About;
+function About() {
+    return (
+      <section>
+        <h2>About Me</h2>
+        <img src="my-app\src\logo.svg" alt="propic" />
+        <p>lorem</p>
+      </section>
+    );
+  }
+  
+
+export default About;
