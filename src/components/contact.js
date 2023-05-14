0ce4dccd46282321c43c0023865b207bfea97c3e
daveS <davesimpcontrol@gmail.com>
2023-05-14 13:27:36 -0600

    added a place to add user name-email and message with server side script as an attempt to actually send the message, still needs validators and may need downgrading to a basic form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,3 +1,82 @@
 import React, { useState } from 'react';
 
-// export default Contact;
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const { name, email, message } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setFormErrors({ ...formErrors, [e.target.name]: '' });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      // Handle form submission here
+      console.log(formData);
+      setFormData({ name: '', email: '', message: '' });
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2>Contact</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className={`form-control ${formErrors.name}`}
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+          />
+          {formErrors.name && (
+            <div className="invalid-feedback">{formErrors.name}</div>
+          )}
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className={`form-control ${formErrors.email}`}
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            className={`form-control ${formErrors.message}`}
+            id="message"
+            name="message"
+            value={message}
+            onChange={handleChange}
+          />
+          {formErrors.message && (
+            <div className="invalid-feedback">{formErrors.message}</div>
+          )}
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
