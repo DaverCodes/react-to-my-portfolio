e0a95ba358017e28460fd99c535b7ce1e608d77c
daveS <davesimpcontrol@gmail.com>
2023-05-14 14:04:18 -0600

    attempting to make the resume viewable as a full image

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,3 +1,13 @@
 import React from "react";
 
-// export default Resume;
+function Resume() {
+  const url = "https://docs.google.com/document/d/1kEdP4fY9lt8w0ULT5dhBIXIZKvl1htF8fNUwOfcHx3g/edit?usp=sharing";
+
+  return (
+    <div>
+      <iframe src={url} title="Resume" width="100%" height="600"></iframe>
+    </div>
+  );
+}
+
+export default Resume;
