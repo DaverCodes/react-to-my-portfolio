95fbf7c93ecce98132bb082bfc16515c7eba1350
daveS <davesimpcontrol@gmail.com>
2023-05-18 23:59:46 -0600

    added picturesm attached them to portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -5,25 +5,26 @@ function Portfolio() {
       <section id="portfolio">
         <h2>Portfolio</h2>
         <div className="project-container">
-          <Project title="OOP: SVG Generator" image="path/to/image1.jpg" deployed="https://github.com/DaverCodes/Super-Very-Good-Image-Maker"/>
-          <Project title="NoSQL: Social Network API" image="path/to/image2.jpg" deployed="https://github.com/DaverCodes/myspace2"/>
-          <Project title="Node.js:README Generator " image="path/to/image3.jpg" deployed="https://github.com/DaverCodes/EasyBakeReadMe"/>
-          <Project title="Interactive Front-End Group Project" image="path/to/image4.jpg" deployed="https://kshakely5280.github.io/project_one/"/>
+          <Project title="OOP: SVG Generator" image="./svgMakerPic.png" deployed="https://github.com/DaverCodes/Super-Very-Good-Image-Maker"/>
+          <Project title="NoSQL: Social Network API" image="./NoSQL.png" deployed="https://github.com/DaverCodes/myspace2"/>
+          <Project title="Node.js:README Generator " image="./READMEGEN.png" deployed="https://github.com/DaverCodes/EasyBakeReadMe"/>
+          <Project title="Interactive Front-End Group Project" image="./project1.png" deployed="https://kshakely5280.github.io/project_one/"/>
         </div>
       </section>
     );
 }
 
 function Project(props) {
-    return (
-      <div className="project">
-        <img src={props.image} alt={props.title} />
-        <div className="project-links">
-          <a href={props.github}>GitHub Repo</a>
-          <a href={props.deployed}>Deployed App</a>
-        </div>
+  return (
+    <div className="project">
+      <img src={props.image} alt={props.title} />
+      <div className="project-links">
+        <a href={props.github}>GitHub Repo</a>
+        <a href={props.deployed} className="deployed">Deployed App</a>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
 
 export default Portfolio;
