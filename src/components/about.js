acd96604efa8f4d1a85839858978337de2b44d29
daveS <davesimpcontrol@gmail.com>
2023-05-18 21:07:17 -0600

    added about me

--- src/components/about.js
+++ src/components/about.js
@@ -4,8 +4,12 @@ function About() {
     return (
       <section>
         <h2>About Me</h2>
-        <img src="my-app\src\logo.svg" alt="propic" />
-        <p>lorem</p>
+        <img src="./meandrico.jpg" alt="Me" />
+                <p>I have an HVAC - maintenance  background and a passion for creating things. Coding has become a fascinating field for me to explore and try new ideas. combines creativity and intellectual stimulation, allowing me to bring concepts to life through software.
+          
+          I'm excited about the possibilities that coding offers, from building user-friendly interfaces to designing efficient algorithms. By combining my HVAC knowledge with coding, I aim to improve efficiency and user experience in the HVAC industry through innovative solutions. Embracing this new journey allows me to learn, grow, and challenge myself while finding fulfillment in creating and experimenting with new technologies.
+          
+          If you're looking for someone who loves learning, brings a fresh perspective, and enjoys creating in the coding world, I would be thrilled to connect and explore collaboration opportunities. </p>
       </section>
     );
   }
